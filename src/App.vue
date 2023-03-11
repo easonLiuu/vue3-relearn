19e892167dda6f5746c578635ae920a92d90e4cb
liujiarui <ljr3289595084@163.com>
2023-03-11 10:29:18 +0800

    feat: add some demos

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,33 @@
 </template>
 
 <script lang="ts">
-import { ref, computed } from 'vue';
+import { computed, reactive, toRefs } from 'vue';
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+}
 
 export default {
   name: 'App',
   setup() {
-    const count = ref(0)
-    const double = computed(() => {
-      return count.value * 2
+    // const count = ref(0)
+    // const double = computed(() => {
+    //   return count.value * 2
+    // })
+    // const increase = () => {
+    //   count.value++
+    // }
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => { data.count++ },
+      double: computed(() => data.count * 2)
     })
-    const increase = () => {
-      count.value++
+    const refData = toRefs(data)
+    //展开的每一项都是响应式的
+    return { 
+      ...refData
     }
-    return { count, increase, double }
   }
 };
 </script>
